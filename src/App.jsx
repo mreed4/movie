8e203c6b34b3e7bd15b2c604de8b9c2193643276
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-01 20:06:50 -0700

    More router work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { AppProvider } from "./AppContext";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import Inputs from "./Inputs";
-import SearchResults from "./SearchResults";
+import Search from "./Search";
+import MoviePage from "./MoviePage";
 
 function App() {
   return (
@@ -10,9 +10,10 @@ function App() {
       <Router>
         <main className="App">
           <Routes>
-            <Route path="/" element={<Inputs />}></Route>
+            <Route path="/" element={<Search />}></Route>
+            <Route path="/movie/:id" element={<MoviePage />}></Route>
+            <Route path="*" element={<h1>404</h1>}></Route>
           </Routes>
-          <SearchResults />
         </main>
       </Router>
     </AppProvider>
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "./AppContext";
+import { Link } from "react-router-dom";
 import Pagination from "./Pagination";
 
 export default function SearchResults() {
@@ -11,7 +12,9 @@ export default function SearchResults() {
         <ol>
           {movies.map((movie, i) => (
             <li key={i}>
-              <img src={movie.Poster !== "N/A" ? movie.Poster : "https://placehold.co/150x250?text=No+Poster"} className="movie-poster" />
+              <Link to="/movie/:id">
+                <img src={movie.Poster !== "N/A" ? movie.Poster : "https://placehold.co/150x250?text=No+Poster"} className="movie-poster" />
+              </Link>
             </li>
           ))}
         </ol>
