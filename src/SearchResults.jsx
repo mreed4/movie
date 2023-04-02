2de0a61646597a44515d28753510d89b79044371
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-02 11:00:12 -0700

    Error handling

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -6,20 +6,20 @@ import Pagination from "./Pagination";
 export default function SearchResults() {
   const { movies, toKebabCase } = useContext(AppContext);
 
-  return (
-    movies.length !== 0 && (
-      <div id="search-results">
-        <ol>
-          {movies.map(({ Poster: src, Title: title, Year: year, imdbID: id }, i) => (
-            <li key={i}>
-              <Link to={`/movie/${id}/${toKebabCase(title)}-${year}`}>
-                <img src={src !== "N/A" ? src : "https://placehold.co/150x250?text=No+Poster"} className="movie-poster" />
-              </Link>
-            </li>
-          ))}
-        </ol>
-        <Pagination />
-      </div>
-    )
+  return movies && movies.length !== 0 ? (
+    <div id="search-results">
+      <ol>
+        {movies.map(({ Poster: src, Title: title, Year: year, imdbID: id }, i) => (
+          <li key={i}>
+            <Link to={`/movie/${id}/${toKebabCase(title)}-${year}`}>
+              <img src={src !== "N/A" ? src : "https://placehold.co/150x250?text=No+Poster"} className="movie-poster" />
+            </Link>
+          </li>
+        ))}
+      </ol>
+      <Pagination />
+    </div>
+  ) : (
+    !movies && "No movies found"
   );
 }
