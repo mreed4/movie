88a8f5124cab4201aabd3f2bb05c34d0c3363692
Matthew <78713317+mreed4@users.noreply.github.com>
2023-07-23 20:41:31 -0700

    Reattempt deployment

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       app: "./src/index.html",
-  //     },
-  //   },
-  // },
-  // server: {
-  //   open: "./src/index.html",
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./index.html",
+      },
+    },
+  },
 });
