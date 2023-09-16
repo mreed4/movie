6d77b2d099a07c1f75ee5567616b7279475198c3
Matthew <78713317+mreed4@users.noreply.github.com>
2023-09-15 21:41:18 -0700

    Move `AppContext`

--- src/components/pages/SearchPage.jsx
+++ src/components/pages/SearchPage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import { useContext } from "react";
-import { AppContext } from "../Contexts/AppContext";
+import { AppContext } from "../AppContext";
 
 import "../../assets/css/SearchPage.css";
 
