403bc1daffcc4cbd9fef621a916ebb09228ead07
Matthew <78713317+mreed4@users.noreply.github.com>
2023-09-15 21:56:18 -0700

    Directory names

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppProvider } from "./Components/AppContext";
+import { AppProvider } from "./Components2/AppContext";
 
 import "./assets/css/index.css";
 
-import App from "./Components/App";
+import App from "./Components2/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
