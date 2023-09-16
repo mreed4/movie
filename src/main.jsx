0ac48c965adc12a353487e2a8b31b096f45bdf6b
Matthew <78713317+mreed4@users.noreply.github.com>
2023-09-15 21:15:04 -0700

    Attempt resolve Netlify issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppProvider } from "./Components/Contexts/AppContext";
 
 import "./assets/css/index.css";
 
-import { AppProvider } from "./Components/Contexts/AppContext";
 import App from "./Components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <AppProvider>
+    <App />
+  </AppProvider>
+  // </React.StrictMode>
 );
